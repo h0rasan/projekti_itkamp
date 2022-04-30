1031043ec819273d384bdc5c3ac937fd1153615c
Seid Jasarevic <jasarevics@live.com>
2022-04-30 18:29:51 +0200

    kalkulator starosti

--- domaci-1/script.js
+++ domaci-1/script.js
@@ -1,5 +1,19 @@
-godine = prompt('koliko godina imas?');
-console.log('imas: ' + godine);
-window.alert(
-    'test'
-);
+let godine = prompt('Koje godine si rodjen?');
+console.log('Imas ', 2021 - godine, 'godina');
+
+let meseci = Number(prompt('Kojeg mjeseca si rodjen? Upisi redni broj mjeseca!'));
+
+if (meseci < 4) {
+    console.log('Imas ', 4 - meseci, 'meseca');
+} else if (meseci === 4) {
+    console.log('Imas 0 meseci');
+} else if (meseci > 4) {
+    console.log('Imas ', 12 - meseci + 4, 'meseci');
+}
+
+let dani = prompt('Kojeg dana si rodjen?');
+if (dani < 30) {
+    console.log('Imas ', 30 - dani, 'dana');
+} else if (dani === 30) {
+    console.log('Imas 0 dana');
+} 
