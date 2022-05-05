ac31ac96b063427417f684c7b01ee7be3439f1f4
Seid Jasarevic <jasarevics@live.com>
2022-05-05 22:06:27 +0200

    cas3

---
+++ cas3/script.js
@@ -0,0 +1,19 @@
+//or (let i=1; i<=10; i++) {
+ //   if (i !==7) {
+ //   console.log(' unosimo policu broj', i) }
+//}
+
+//for (let i=1; i<=10; i++) {
+ //   if (i === 7) {
+ //       continue;
+ //   }
+ //   console.log(i);
+//}
+
+sum = 0;
+for (i=0; i<=20; i=i+2) {
+    sum = sum + i;
+    console.log(sum);
+}
+
+console.log(sum);
