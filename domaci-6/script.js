a0cd61012c8937890dfe458c5047ec243f33d02b
Seid Jasarevic <jasarevics@live.com>
2022-05-15 14:12:11 +0200

    while petlja zadaci

---
+++ domaci-6/script.js
@@ -0,0 +1,37 @@
+// PRVI ZADATAK
+
+// num = parseInt(prompt('Unesite neki broj'));
+// sum = 0;
+// while (num >= 0) {
+//     sum = sum + num;
+//     num = parseInt(prompt('Broj je pozitivan. Unesite drugi broj'));
+// }  console.log('Broj je negativan. Zbir unetih brojeva je:', sum);
+
+
+///////////////////////////////////////////////////
+
+
+//DRUGI ZADATAK
+
+// unos = prompt('Unesite neko ime');
+
+// while (unos != 'END') {
+//     console.log(unos);
+//     unos = prompt('Unesite neko ime. Za prekid petlje upisite END');
+// } console.log('GOTOVO!!!');
+
+
+////////////////////////////////////////////////////
+
+
+//TRECI ZADATAK
+
+ocene = parseInt(prompt('Unesite ocenu!'));
+zbirOcena = 0;
+brojOcena = 0;
+
+while (ocene > 0) {
+    zbirOcena += ocene;
+    brojOcena++;
+    ocene = parseInt(prompt('Unesite ocenu!. Za prekid petlje unesite negativan broj'));
+} console.log('Prosjecna ocena je: ',zbirOcena / brojOcena);
