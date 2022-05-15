aced89c3ec5881885a2e8e8713fe7fcb6b86249f
Seid Jasarevic <jasarevics@live.com>
2022-05-15 14:13:55 +0200

    while petlja

---
+++ cas6/script.js
@@ -0,0 +1,18 @@
+
+
+// unos = parseInt(prompt('unesite neki broj'));
+
+// for (var i = 1; i <= unos; i++) {
+//     str = " ";
+//     for (var j = 0; j < i; j++) {
+//         str += i;
+//         } console.log(str);
+// }
+
+
+rows = parseInt(prompt('unesite broj redova'));
+sp 
+for (var i = 0; i < rows; i++) {
+
+}
+
