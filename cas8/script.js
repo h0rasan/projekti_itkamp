7f0d6fc3fc2cf185bdff935b36fc2846a0e2308f
Seid Jasarevic <jasarevics@live.com>
2022-05-19 19:28:56 +0200

    kalkulator

---
+++ cas8/script.js
@@ -0,0 +1,22 @@
+
+// operator = prompt('unesite operator');
+// numA = parseInt(prompt('Unesite prvi broj'));
+// numB = parseInt(prompt('Unesite drugi broj'));
+
+// while (operator != 0 || operator != 'exit') {
+//     if (operator === '+') {
+//         console.log('Zbir dva broja je: ', numA + numB);
+//     } else if (operator === '-') {
+//         console.log('Razlika dva broja je: ', numA - numB);
+//     } else if (operator === '*') {
+//         console.log('Proizvod dva broja je: ', numA * numB);
+//     } else if (operator === '/') {
+//         console.log('Kolicnik dva broja je: ', numA / numB);
+//     } else {console.log('Izasli ste iz aplikacije')}
+// };
+
+
+while (true) {
+    operacija = prompt('Unesite operaciju');
+    if (operacija === 'exit' ||)
+}
