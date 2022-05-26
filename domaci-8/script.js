956c8b9eba12a47d05c6fe028f1e44db7b6cc048
Seid Jasarevic <jasarevics@live.com>
2022-05-26 12:09:39 +0200

    array methods

---
+++ domaci-8/script.js
@@ -0,0 +1,76 @@
+// arr = [];
+// unos = parseInt(prompt('Koliko brojeva unosite?'));
+// for (i = 0; i < unos; i++) {
+//     niz = parseInt(prompt('Unesite broj.'));
+//     arr.push(niz);
+    
+// }
+
+// console.log('Najmanji broj je: ', Math.min(...arr));
+// console.log('Najveci broj je: ', Math.max(...arr));
+
+
+/////////////////////////////////////////////////////////
+
+
+// arr = [];
+// unos = parseInt(prompt('Koliko brojeva unosite?'));
+// for (i = 0; i < unos; i++) {
+//     niz = parseInt(prompt('Unesite broj.'));
+//     arr.push(niz); 
+// }
+
+
+// first = Math.max(...arr);
+// index = arr.indexOf(arr);
+// arr.splice(index, 1);
+// console.log('Drugi najveci broj je: ', Math.max(...arr));
+
+
+///////////////////////////////////////////////////////////
+
+
+// arr1 = [1, 2, 3, 4, 5];
+// arr2 = [1, 2, 3, 4];
+
+// if (arr1.length === arr2.length) {
+//     console.log('Nizovi su jednaki');
+// } else {console.log('Nizovi nisu jednaki');}
+
+
+///////////////////////////////////////////////////////////
+
+
+// arr = [];
+// unos = parseInt(prompt('Provera razlike vrednosti u nizu. Koliko brojeva unosite?'));
+// for (i = 0; i < unos; i++) {
+//     niz = parseInt(prompt('Unesite broj.'));
+//     arr.push(niz); 
+// }
+
+// console.log('Razlika je: ', Math.max(...arr) - Math.min(...arr));
+
+
+//////////////////////////////////////////////////////////////
+
+
+
+// neparni = [];
+// parni = [];
+// zbirParnih = 0;
+// zbirNeparnih = 0;
+// unos = parseInt(prompt('Koliko brojeva unosite?'));
+// for (i = 0; i < unos; i++) {
+//     niz = parseInt(prompt('Unesite broj.'));
+//     if (niz % 2 === 0) {
+//         parni.push(niz);
+//         zbirParnih += parni.length[niz];
+//     } else { 
+//         neparni.push(niz);
+//         zbirNeparnih += neparni[niz];
+//     }
+    
+    
+// }
+// console.log(zbirParnih);
+
