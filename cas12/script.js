67c18d3d37a8e5f4e786b20697f006a1cd7f5253
Seid Jasarevic <jasarevics@live.com>
2022-06-07 15:15:54 +0200

    array metode vezbe

---
+++ cas12/script.js
@@ -0,0 +1,19 @@
+
+arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+nRed1 = 0;
+nRed2 = 0;
+nRed3 = 0;
+for (i = 0; i < arr.length; i++) {
+    red = "";
+    for (j = 0; j < arr[i].length; j++) {
+        red += arr[i][j] + " ";
+       
+        
+    } console.log(red);
+    nRed1 +=arr[i][0];
+    nRed2 += arr[i][1];
+    nRed3 += arr[i][2];
+    
+}
+console.log(nRed1, nRed2, nRed3);
